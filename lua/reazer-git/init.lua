2516846517178be3cbcb8ace9dc094795e13b6b7
Sandro Sauer <sauer@creative-network.biz>
2025-01-28 13:54:10 +0100

    first commit

---
+++ lua/reazer-git/init.lua
@@ -0,0 +1,19 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+
+local M = {}
+
+-- our picker function: colors
+M.colors = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "git status",
+    finder = finders.new_table {
+      results = { "red", "green", "blue", "test.lua" }
+    },
+    sorter = conf.generic_sorter(opts),
+  }):find()
+end
+
+return M
---
+++ lua/telescope/_extensions/reazer-git.nvim.lua
@@ -0,0 +1,11 @@
+local has_telescope, telescope = pcall(require, "telescope")
+
+if not has_telescope then
+    error("reazer-git.nvim requires nvim-telescope/telescope.nvim")
+end
+
+return telescope.register_extension {
+  exports = {
+    reazer_git = require("reazer-git.nvim")
+  },
+}
